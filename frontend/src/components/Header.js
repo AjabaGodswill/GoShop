c7c2cd52a61a122fe28b79f0460401155747f94e
AjabaGodswill <AjabaGodswill@gmail.com>
2023-02-19 20:53:51 +0100

    Created Home page and added products to the hompage

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,36 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react'
-import { Nav,Navbar,Container } from "react-bootstrap";
+import { Nav, Navbar, Container } from "react-bootstrap";
+// import logo from '../../public/images/logo.png'; 
 
 
 const Header = () => {
   return (
     <header> <Navbar bg="light" expand="lg" collapseOnSelect>
     <Container>
-      <Navbar.Brand href="/">GoShop</Navbar.Brand>
+        <Navbar.Brand href="/">
+          GoShop
+        {/* <img src={logo}  alt= 'logo'/> */}
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+          <Nav.Link href="/">
+             Home
+          </Nav.Link>
+          <Nav.Link href="/shop">
+            Shop
+          </Nav.Link>
+          <Nav.Link href="/collections">
+             Collections
+          </Nav.Link>
+          <Nav.Link href="/order-tracking">
+             Order Tracking
+          </Nav.Link>
+          <Nav.Link href="/blog">
+             Blog
+          </Nav.Link>
+        </Nav>
         <Nav className="ms-auto">
             <Nav.Link href="/search">
               <i className = 'fas fa-search'></i>
---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: "1",
+    name: "Airforce one",
+    image: "/images/Nike Airforce 1s.png",
+    description:
+      "A Classic Since 1982 Designed by Bruce Kilgore and introduced in 1982, the Air Force 1 was the first ever basketball shoe to feature Nike Air technology, revolutionizing the game and sneaker culture forever.",
+    brand: "Nike",
+    category: "Electronics",
+    price: 400,
+    countInStock: 0,
+    rating: 4.2,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Nike Airmax",
+    image: "/images/22 1.png",
+    description:
+      "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Shoes",
+    price: 173,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Nike Arimax 270",
+    image: "/images/transparent-nike-shoe-png-air-max-270-ultra-marine-png-download-kindpng 1.png",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Nike Arimax 2021",
+    image: "/images/Airmax.png",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    brand: "Sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/NEWW 1.png",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/Bg(3).png",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
